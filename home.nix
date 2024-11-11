ffb1beda64b5ed58a68ab8082b6c63b6ae2f1ad0
kisan <kisansakthi@gmail.com>
2024-11-12 01:51:23 +0530

    add few pkgs

--- home.nix
+++ home.nix
@@ -21,6 +21,10 @@
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     pkgs.hello
+    pkgs.tor-browser
+    pkgs.arandr
+    pkgs.telegram-desktop
+    pkgs.nicotine-plus
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
